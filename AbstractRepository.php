5d3bfc7b0d48346895931885a545a0aad3c34f59
David Bruchmann <david.bruchmann@gmail.com>
2023-02-21 16:31:17 +0700

    remove objectManager

--- AbstractRepository.php
+++ AbstractRepository.php
@@ -3,7 +3,6 @@
 namespace Vendor\ExtensionSignature\Domain\Repository;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Object\ObjectManager;
 use TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser;
 use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
@@ -11,8 +10,7 @@ class AbstractRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
 {
     protected function debugQuery($query, $result = null, $debugHeader = ''): void
     {
-        $objectManager = $this->objectManager ?: GeneralUtility::makeInstance(ObjectManager::class);
-        $dbParser = $objectManager->get(Typo3DbQueryParser::class);
+        $dbParser = GeneralUtility::makeInstance(Typo3DbQueryParser::class);
         $doctrineQueryBuilder = $dbParser->convertQueryToDoctrineQueryBuilder($query);
         $sql = $doctrineQueryBuilder->getSQL();
         $parameters = $doctrineQueryBuilder->getParameters();

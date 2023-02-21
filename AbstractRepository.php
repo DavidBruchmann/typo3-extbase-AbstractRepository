9aa8f51a4c48f638c07b7f3933644d1ec2a298bb
David Bruchmann <david.bruchmann@gmail.com>
2023-02-21 16:27:39 +0700

    initial commit

---
+++ AbstractRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Vendor\ExtensionSignature\Domain\Repository;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Object\ObjectManager;
+use TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbQueryParser;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+
+class AbstractRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
+{
+    protected function debugQuery($query, $result = null, $debugHeader = ''): void
+    {
+        $objectManager = $this->objectManager ?: GeneralUtility::makeInstance(ObjectManager::class);
+        $dbParser = $objectManager->get(Typo3DbQueryParser::class);
+        $doctrineQueryBuilder = $dbParser->convertQueryToDoctrineQueryBuilder($query);
+        $sql = $doctrineQueryBuilder->getSQL();
+        $parameters = $doctrineQueryBuilder->getParameters();
+        $debugHeader .= ($debugHeader ? ' // ' : '') . __METHOD__ . ':' . __LINE__;
+        DebuggerUtility::var_dump([
+            'sql' => $sql,
+            'parameters' => $parameters,
+            'result' => $result,
+            // 'backtrace' => debug_backtrace(),
+        ], $debugHeader);
+    }
+}
